ae884560566d21105b5c445b52cde88ffa56a98b
Philippe MEYER <p.meyer@mappingcontrol.com>
2022-05-24 00:05:53 +0200

    the map now moves when the player moves close to the canvas edges

--- js/sketch.js
+++ js/sketch.js
@@ -1,28 +1,30 @@
-let w2,h2,k90degres,k60degres,k45degres,k180degres,k270degres,k360degres,k80degres,k280degres,camFov,focalW,focalH,zoom,focalAverage;
+let w2,h2,w4,h4,k90degres,k60degres,k45degres,k180degres,k270degres,k360degres,k80degres,k280degres,camFov,focalW,focalH,zoom,focalAverage;
 let needUpdate,saveContext,context,_camera,mode,things,debugMode,scribble;
-let world1;
+let worldModel,gameLoaded;
 
 function setup() {
     debugMode = false;
-
-	loadJSON('/files/world1.json', world1 => {
-		console.log(world1)
-		things = setNotMobs(world1);
+	gameLoaded = false;
+	loadJSON('/files/world1.json', result => {
+		worldModel = {...result}
+		things = setNotMobs(worldModel);
+		things.forEach(function(t){
+			t.init();
+		});
+		createCanvas(windowWidth, windowHeight);
+		setUtilValues();
+		translate(width / 2, height / 2);
+		context = drawingContext;
+		_camera = new Kamera(0.2,10,toradians(90));
+		setKeyDown();
+		gameLoaded = true;
 	  });
-    createCanvas(windowWidth, windowHeight);
-    setUtilValues();
-    translate(width / 2, height / 2);
-	context = drawingContext;
-	
-	_camera = new Kamera(0.2,10,toradians(90));
-	setKeyDown();
-	
   }
 
   function draw() {
+	if(!gameLoaded) return;
 	translate(width / 2, height / 2);
 	clear();
-	//context.clearRect(-w2 , -h2, width, height);
 	context.fillStyle="rgb(185,183,184)"; 
 	context.rect(-w2 , -h2, width, height);
 	context.fill();
@@ -35,6 +37,14 @@ function setup() {
 
 
 // Utilities
+
+function drawingPositionGet(truePosition) {
+	return {
+		"x": worldModel && worldModel.currentCenter ? truePosition.x - worldModel.currentCenter.x : truePosition.x,
+		"y": worldModel && worldModel.currentCenter ? truePosition.y - worldModel.currentCenter.y : truePosition.y
+	};
+}
+
   function setKeyDown(){
     document.addEventListener("keydown", function(event) {
 		switch(event.code) {
@@ -88,6 +98,8 @@ function setup() {
   function setUtilValues(){
 	w2 = width/2;
 	h2 = height/2;
+	w4 = width/4;
+	h4 = height/4;
 	k90degres = toradians(90);
 	k60degres = toradians(60);
 	k45degres = toradians(45);
@@ -116,66 +128,58 @@ function todegrees(radians) {
 function Kamera(rotStep,walkStep,rotation) {
 	this.knownThings = [];
 	this.rotation = rotation ? rotation : 0; 
-	this.position = {x:0,y:0,z:0};
-	this.previousLocation = {x:0,y:0,z:0}; 
-	this.antePenultLocation = {x:0,y:0,z:0}; 
+	this.position = {x:0,y:0};
+	this.previousLocation = {x:0,y:0}; 
+	this.antePenultLocation = {x:0,y:0}; 
 
 	this.sightWidth = toradians(90);
 	this.sightLength = 200;
 	this.walkStep = walkStep;
 	this.rotStep = rotStep;
 	this.bodyRadius = 20;
+
 	this.turn = function(amount){ // -1 or +1
 		this.rotation -= this.rotStep*amount;
 		if(this.rotation<0) this.rotation  += k360degres;
-		if(this.rotation >k360degres ) this.rotation = 0;
+		if(this.rotation > k360degres ) this.rotation = 0;
 	}
+
 	this.walk = function(amount){// -1 or +1
+		var self = this;
 		// Calculate new position considering the amount, the position and the direction	
 		this.savePosition();
 		var dirx = Math.cos(this.rotation);
 		var dirz = - Math.sin(this.rotation);
-		this.position.x = Math.floor(this.position.x + (dirx * amount * this.walkStep)); 
-		this.position.z = Math.floor(this.position.z + (dirz * amount * this.walkStep));
+		self.position.x = Math.floor(self.position.x + (dirx * amount * self.walkStep)); 
+		self.position.y = Math.floor(self.position.y + (dirz * amount * self.walkStep));
+
+		let drawPos = drawingPositionGet(self.position);
+		if(drawPos.x > (w2 - w4)){
+			worldModel.currentCenter.x += (self.position.x - self.previousLocation.x);
+		}else if(drawPos.x < (w4 - w2)){
+			worldModel.currentCenter.x -= (self.previousLocation.x - self.position.x);
+		}
+
+		if(drawPos.y > (h2 -h4)){
+			worldModel.currentCenter.y += (self.position.y - self.previousLocation.y);
+		}else if(drawPos.y < (h4 - h2)){
+			worldModel.currentCenter.y -= (self.previousLocation.y - self.position.y);
+		}
 	}
 
 	this.draw = function(){
 		var self = this;
 		
 	    self.checkCollisions();
-		
-		self.drawSoil();
-		
 		self.drawCross();
-
-		self.drawScanner();
-
 		self.drawCamera();
 	}
-	
-	this.drawSoil=function(){
-		var self = this;
-		var soilColor = color(20,230,160); 
-		things.forEach(function(x){
-			var alpha = 1-(x.distance/400);
-				if (alpha < 0.1) alpha = 0.1;
-				context.globalAlpha=alpha;				
-				context.beginPath();
-				context.strokeStyle="black"; 
-				context.fillStyle=soilColor; 
-				fill(soilColor);
 
-				context.arc(0, 0, x.distance, 0, Math.PI * 2, true);
-				context.closePath();
-				context.stroke();	
-				context.fill();				
-			}); 
-	}
 	this.checkCollisions = function () {
 		var self = this;
 		things.forEach(function (x) {
 			var poly = x.geometry.data2D;
-			if (collideCirclePoly(self.position.x, self.position.z, self.bodyRadius * 2, poly)) {
+			if (collideCirclePoly(self.position.x, self.position.y, self.bodyRadius * 2, poly)) {
 				self.restorePosition();
 			}
 		});
@@ -195,10 +199,10 @@ function Kamera(rotStep,walkStep,rotation) {
 			y: camSin * 50
 		};
 
-		var west = simpleRotate(vectorCam, k90degres);
-		var east = simpleRotate(vectorCam, -k90degres);
-		var north = vectorCam;
-		var south = simpleRotate(vectorCam, -k180degres);
+		var west = drawingPositionGet(simpleRotate(vectorCam, k90degres));
+		var east = drawingPositionGet(simpleRotate(vectorCam, -k90degres));
+		var north = drawingPositionGet(vectorCam);
+		var south = drawingPositionGet(simpleRotate(vectorCam, -k180degres));
 
 
 		context.beginPath();
@@ -223,144 +227,60 @@ function Kamera(rotStep,walkStep,rotation) {
 	}
 	
 	this.drawCamera=function(){
-			context.save();
-
-			context.globalAlpha=1;	
-			
-			context.beginPath();
-			context.strokeStyle="black"; 
-			context.fillStyle="red"; 
-			
-			context.beginPath();
-			context.arc(_camera.position.x, _camera.position.z, this.bodyRadius,0, 2*Math.PI,false);
-			context.closePath();
-			context.stroke();
-			context.fill();
-
-			//var messagePosition = _camera.position.x + "," + _camera.position.z;
-			var camCos = Math.cos(_camera.rotation);
-			var camSin = -Math.sin(_camera.rotation);
-			var vectorCam = {x:camCos*30,y:camSin*30};
-			
-			context.fillStyle="white"; 
-			context.beginPath();
-			//drawArrow(context,_camera.position.x,_camera.position.z,_camera.position.x+vectorCam.x,_camera.position.z+vectorCam.y);
-			context.arc(_camera.position.x+(vectorCam.x*0.4), _camera.position.z+(vectorCam.y*0.4), (this.bodyRadius/3),0, 2*Math.PI,false);
-			context.closePath();
-			context.stroke();
-			context.fill();
-
-			context.fillStyle="black"; 
-			context.beginPath();
-			context.arc(_camera.position.x+(vectorCam.x*0.5), _camera.position.z+(vectorCam.y*0.5), (this.bodyRadius/4),0, 2*Math.PI,false);
-			context.closePath();
-			context.stroke();
-			context.fill();
-
-			context.strokeStyle="darkred"; 
-			context.fillStyle="black"; 
-			/*
-			context.beginPath();
-			context.fillText(messagePosition, _camera.position.x -7, _camera.position.z -this.bodyRadius/2);
-			context.fillText(Math.floor(_camera.rotation * 180 / Math.PI) +" °", _camera.position.x -6, _camera.position.z +this.bodyRadius/2);
-
-			context.closePath();
-			context.stroke();
-			*/
-		
-
-			context.restore();
-	}
-
-	this.drawScanner=function(){
+		let drawPos = drawingPositionGet(this.position);
 		context.save();
-		context.globalAlpha=0.35;
-		context.beginPath();
-		var rotationLeftLimit = _camera.rotation-this.sightWidth/2;
-		var rotationRightLimit = _camera.rotation+this.sightWidth/2;
 
-		context.strokeStyle="rgb(255,255,0)"; 
-		var ray;
-		things.forEach(function(x){
-			x.hit = false;
-			x.hitAngles.length = 0;
-		});
-		var relativeAngle = - this.sightWidth/2;
-		var step = 0.02;
-		var rayLength =  this.sightLength-this.bodyRadius;
-		for(var i = rotationLeftLimit;i <= rotationRightLimit;i+=step){
-			camCos = Math.cos(i);
-			camSin = -Math.sin(i);
-			
-			var start = {"x":_camera.position.x+camCos*this.bodyRadius,"y":_camera.position.z+camSin*this.bodyRadius};
-			context.moveTo(start.x, start.y);
-			
-			ray= {x:camCos*rayLength,y:camSin*rayLength};
-			var end = {"x":start.x+ray.x,"y":start.y+ray.y};
-			context.lineTo(end.x,end.y);
-			things.forEach(function(x){
-				
-				var poly = x.geometry.data2D;
-			
-				if(collideLinePoly(start.x,start.y,end.x,end.y,poly)){
-					x.hit = true;
-					x.hitAngles.push(relativeAngle);
-				}					
-			});
-			relativeAngle += step;
-		}
-		things.forEach(function(x){
-			if(x.hit){
-				var nrHits = x.hitAngles.length;
-				if(nrHits==0){
-					x.hit = false;
-				}else if(nrHits==1){
-					x.hitMiddleAngle = x.hitAngles[0];
-				}else{
-					x.hitMiddleAngle = x.hitAngles[Math.floor((nrHits-1)/2)];
-				}
-			}
+		context.globalAlpha=1;	
 		
-		});
+		context.beginPath();
+		context.strokeStyle="black"; 
+		context.fillStyle="red"; 
 		
-		context.moveTo(_camera.position.x, _camera.position.z);
-		camCos = Math.cos(rotationRightLimit);
-		camSin = -Math.sin(rotationRightLimit);
-		ray= {x:camCos*this.sightLength,y:camSin*this.sightLength};
-		context.lineTo(_camera.position.x+ray.x,_camera.position.z+ray.y);
+		context.beginPath();
+		context.arc(drawPos.x, drawPos.y, this.bodyRadius,0, 2*Math.PI,false);
 		context.closePath();
 		context.stroke();
+		context.fill();
 
-		context.beginPath();
-		context.moveTo(_camera.position.x, _camera.position.z);
-		context.arc(_camera.position.x, _camera.position.z, this.sightLength, -rotationRightLimit, -rotationLeftLimit,false);
+		var camCos = Math.cos(_camera.rotation);
+		var camSin = -Math.sin(_camera.rotation);
+		var vectorCam = {x:camCos*30,y:camSin*30};
 		
+		context.fillStyle="white"; 
+		context.beginPath();
+		context.arc(drawPos.x+(vectorCam.x*0.4),drawPos.y+(vectorCam.y*0.4), (this.bodyRadius/3),0, 2*Math.PI,false);
 		context.closePath();
 		context.stroke();
-				
+		context.fill();
+
+		context.fillStyle="black"; 
+		context.beginPath();
+		context.arc(drawPos.x+(vectorCam.x*0.5), drawPos.y+(vectorCam.y*0.5), (this.bodyRadius/4),0, 2*Math.PI,false);
+		context.closePath();
+		context.stroke();
+		context.fill();
+
+		context.strokeStyle="darkred"; 
+		context.fillStyle="black"; 
+
 		context.restore();
-
-	}	
+	}
 	
 	this.savePosition = function(){
 		
 		this.antePenultLocation.x = this.previousLocation.x;
 		this.antePenultLocation.y = this.previousLocation.y;
-		this.antePenultLocation.z = this.previousLocation.z;
 		
 		this.previousLocation.x = this.position.x;
 		this.previousLocation.y = this.position.y;
-		this.previousLocation.z = this.position.z;
 	}
 	
 	this.restorePosition = function(){
 		this.position.x = this.previousLocation.x;
 		this.position.y = this.previousLocation.y;
-		this.position.z = this.previousLocation.z;
 		
 		this.previousLocation.x = this.antePenultLocation.x;
 		this.previousLocation.y = this.antePenultLocation.y;
-		this.previousLocation.z = this.antePenultLocation.z;
 	}
 
 }
@@ -391,25 +311,32 @@ function PolyThing(size, distance, angleToOrigine, innerRotation, name,color,mat
     this.hitAngles = [];
     this.hitMiddleAngle = 0;
 	this.color= color;
-    var cos = Math.cos(this.angleToOrigine);
-    var sin = -Math.sin(this.angleToOrigine);
 
-    // the real position according to origin point
-    this.positionAbsolute.x = Math.floor(cos * distance);
-    this.positionAbsolute.y = Math.floor(sin * distance);
-
-	this.geometry.data2D.forEach((pt)=>{
-		pt = simpleRotate(pt,this.innerRotation);
-	});
-
-	this.geometry.data2D.forEach((pt)=>{
-		pt.x = this.positionAbsolute.x + (this.half * pt.x);
-		pt.y = this.positionAbsolute.y + (this.half * pt.y);
-	});
+	this.init = function(){
+		let self = this;
+		var cos = Math.cos(self.angleToOrigine);
+		var sin = -Math.sin(self.angleToOrigine);
+	
+		// the real position according to origin point
+		self.positionAbsolute.x = Math.floor(cos * self.distance);
+		self.positionAbsolute.y = Math.floor(sin * self.distance);
+	
+		let rotatedPoints = self.geometry.data2D.map((pt)=>{
+			return simpleRotate(pt,self.innerRotation);
+		});
+		self.geometry.data2D = [...rotatedPoints];
+	
+		self.geometry.data2D.forEach((pt)=>{
+			pt.x = self.positionAbsolute.x + (self.half * pt.x);
+			pt.y = self.positionAbsolute.y + (self.half * pt.y);
+		});
+	}
+   
 
     this.draw = function () {
         // the _camera moves : the objects stay stationnary so the positionRelative == positionAbsolute
         var self = this;
+		/*
 		var isVisible = false;
 		if(_camera.knownThings.indexOf(self.name) != -1){
 			isVisible = true;
@@ -419,6 +346,8 @@ function PolyThing(size, distance, angleToOrigine, innerRotation, name,color,mat
 				isVisible = true;
 			}
 		}
+		*/
+		isVisible = true;
 
 		if(!isVisible) return;
 
@@ -428,26 +357,22 @@ function PolyThing(size, distance, angleToOrigine, innerRotation, name,color,mat
         context.fillStyle = "rgb(20,230,160)";
         context.beginPath();
 
-        context.moveTo(self.geometry.data2D[0].x, self.geometry.data2D[0].y);
+		let drawPos = drawingPositionGet(self.geometry.data2D[0]);
+
+        context.moveTo(drawPos.x, drawPos.y);
 		self.geometry.data2D.forEach((pt)=>{
-			context.lineTo(pt.x, pt.y);
+			drawPos = drawingPositionGet(pt);
+			context.lineTo(drawPos.x, drawPos.y);
 		});
 
         context.closePath();
         context.stroke();
-        if (self.hit) {
-            context.fillStyle = self.color;
-            context.fill();
-            context.fillStyle = "black";
-			if(debugMode){
-            	context.fillText(Math.floor(todegrees(self.hitMiddleAngle)) + " °", self.topRight.x + 2, self.topRight.y - 2);
-			}
-        }
-
+		context.fillStyle = self.color;
+		context.fill();
+		context.fillStyle = "black";
         context.globalAlpha = 1;
         context.restore();
     }
-
 }
 
 function simpleRotate(point,angle){
